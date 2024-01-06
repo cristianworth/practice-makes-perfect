f6bfc5f6df9fc5e0e85379fbcaf2db328119d0fe
cristianworth <cristianweissmantel@gmail.com>
2024-01-05 23:58:36 -0300

    feat: Add list gacha functionality

--- resin-management/js/index.js
+++ resin-management/js/index.js
@@ -1,3 +1,10 @@
+var allGachas = [
+    { id: 1, name: 'Genshin Impact'},
+    { id: 2, name: 'Honkai Impact 3rd'},
+    { id: 3, name: 'Honkai Star Rail'},
+    { id: 4, name: 'Punishing Gray Raven'},
+];
+
 function editGacha() {
     console.log('Edit not implemented yet');
 }
@@ -6,22 +13,33 @@ function deleteGacha() {
     console.log('Remove not implemented yet');
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-    let formCreate = document.getElementById("form-create");
-    let gachaName = document.getElementById("name");
+function listGachas() {
+    allGachas.forEach(e => {
+        addNewGachaToList(e.name);
+    });
+}
+
+function addNewGachaToList(gachaName) {
     let gachasList = document.getElementById("gachas-list");
+    let index = gachasList.children.length + 1;
+
+    gachasList.innerHTML += `
+        <li class="gacha-item">
+            <p>${gachaName}</p>
+            <button id="${index}Edit" class="button-item" onclick="editGacha()">Edit</button>
+            <button id="${index}Delete" class="button-item" onclick="deleteGacha()">Delete</button>
+        </li>
+        `;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    let formCreate = document.getElementById("form-create");
+    let gachaName = document.getElementById("name").value;
+
+    listGachas();
 
     formCreate.addEventListener("submit", (e) => {
         e.preventDefault();
-        
-        console.log(gachaName.value);
-
-        gachasList.innerHTML += `
-        <li class="gacha-item">
-            <p>${gachaName.value}</p>
-            <button class="button-item" onclick="editGacha()">Edit</button>
-            <button class="button-item" onclick="deleteGacha()">Delete</button>
-        </li>
-        `;
+        addNewGachaToList(gachaName)
     });
 });
